f795de6cc819e5f462af4ceac8800fbd03850eca
dill-larson <dillonlouislarson@gmail.com>
2021-02-22 19:08:15 -0800

    ✨ added table to display response from API

--- frontend/src/home/Home.js
+++ frontend/src/home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Container, Form } from 'react-bootstrap';
+import { Button, Container, Form, Table } from 'react-bootstrap';
 import { Formik } from 'formik';
 import * as yup from 'yup';
 import axios from 'axios';
@@ -14,17 +14,27 @@ export default class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            response: ''
-        }
+            response: [
+                    {
+                       "Company":"Apple",
+                       "Position":"Software Engineer, Apple Pay",
+                       "Location":"",
+                       "Link":"https://www.linkedin.com/jobs/view/software-engineer-apple-pay-at-apple-2420590319?refId=ae919a2c-a389-4f43-8c0f-2b9d57f003af&amp;trackingId=gy0uIEWXP%2BUBLcTRecaRtw%3D%3D&amp;position=1&amp;pageNum=0&amp;trk=public_jobs_job-result-card_result-card_full-click",
+                       "Time":"2 days ago"
+                    }
+                ]
+            }
     }
 
     handleSubmit(value) {
         console.log(value);
-        //Fake RESTful API
-        axios.get(`https://jsonplaceholder.typicode.com/posts/1`)
+        axios.get(value)
             .then(res => {
-            const response = res.data;
-            this.setState({ response });
+                const response = res.data;
+                this.setState({ response });
+            })
+            .catch(error => {
+                console.error(error);
             });
     }
 
@@ -65,7 +75,29 @@ export default class Home extends React.Component {
                     )}
                 </Formik>
                 <h6>Response:</h6>
-                <p>{JSON.stringify(this.state.response)}</p>
+                {/* <p>{JSON.stringify(this.state.response)}</p> */}
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Company</th>
+                            <th>Position</th>
+                            <th>Link</th>
+                            <th>Date Posted</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.response.map((requisition, index) => {
+                            return (<tr>
+                                <td>{index+1}</td>
+                                <td>{requisition.Company}</td>
+                                <td>{requisition.Position}</td>
+                                <td><a href={requisition.Link}>Opportunity at {requisition.Company}</a></td>
+                                <td>{requisition.Time}</td>
+                            </tr>)
+                        })}
+                    </tbody>
+                </Table>
             </Container>
         );
     }
