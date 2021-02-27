e1bf94031fa55a4dab2c705a535c380b906c20ac
dill-larson <dillonlouislarson@gmail.com>
2021-02-27 15:11:57 -0800

    added routing for signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.scss';
 
-import Home from './home/Home';
+import Home from './Home/Home';
+import SignUp from './SignUp/SignUp';
 
 function App() {
   return (
     <Router>
         <Switch>
             <Route exact path="/" component={Home}/>
+            <Route exact path="/signup" component={SignUp}/>
         </Switch>
     </Router>
   );
