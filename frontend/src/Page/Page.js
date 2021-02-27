7591292177f4dd5dceccb2782c44d039abd0d9b6
dill-larson <dillonlouislarson@gmail.com>
2021-02-27 15:18:15 -0800

    🐛 page now properly exported

--- frontend/src/Page/Page.js
+++ frontend/src/Page/Page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 
-export function Page(props) {
+export default function Page(props) {
     return (
         <Container>
             {props.children}
