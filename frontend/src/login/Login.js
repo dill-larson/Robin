0fbaa8cd7260f48757581235b5b0c435961656bf
Laura Hernandez <laura.hernandezrincon@sjsu.edu>
2021-03-01 10:45:58 -0800

    implemented formik and error control

--- frontend/src/login/Login.js
+++ frontend/src/login/Login.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { Button, Row, Col, Container, Form, Table } from 'react-bootstrap';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import axios from 'axios';
 import Logo from "../illustrations/Logo"
 import Illustration from "../illustrations/Login_illustration"
 
 const pageStyle = {
-    color:"white"
+    color:"white",
+    height:"100vh",
+    backgroundColor:"#d8d8e0"
 }
-
-
 const buttonStyle = {
     color: "#133c8f",
     borderRadius: "15px",
@@ -18,8 +21,8 @@ const buttonStyle = {
 export default class Login extends React.Component{
     render(){
         return (
-            <div style={{height:"100vh", backgroundColor:"#d8d8e0"}}>
-                <Container style={pageStyle}>
+            <div style={pageStyle}>
+                <Container>
                 <Col><Logo size="150px"></Logo></Col>
                 <Row lg={2}>                
                     <Col >
@@ -29,27 +32,82 @@ export default class Login extends React.Component{
                     </Col>
                     <Col > 
                         <Col >
-                            <Form>
+                        <Formik 
+                            initialValues={{
+                                email: '',
+                                password: ''
+                            }}
+                            validate={values => {
+                                const errors = {};
+                                if (!values.email) {
+                                  errors.email = 'Required';
+                                } else if (
+                                  !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                                ) {
+                                  errors.email = 'Invalid email address';
+                                }
+                                if (!values.password) {
+                                    errors.password = 'Required';
+                                  }
+                                return errors;
+                              }
+                            
+                            }
+                              onSubmit={(values, { setSubmitting }) => {
+                                setTimeout(() => {
+                                  alert(JSON.stringify(values, null, 2));
+                                  setSubmitting(false);
+                                }, 400);
+                              }}
+                        >
+                            {({
+                                values,
+                                errors,
+                                touched,
+                                handleChange,
+                                handleBlur,
+                                handleSubmit,
+                                isSubmitting,
+                            }) => (
+                            <Form onSubmit={handleSubmit}>
                                 <h1 style={{color:"#6153ae",fontSize: "3.5em"}}>Login</h1>
-                            <Form.Group controlId="formEmail">
-                                <Form.Label >Email address</Form.Label>
-                                    <Form.Control type="email" style={{borderRadius:"10px", width:"80%"}} />
-                                </Form.Group>
-                                <Form.Group controlId="formPassword">
-                                    <Form.Label >Password</Form.Label>
-                                    <Form.Control type="password" style={{borderRadius:"10px", width:"80%"}} />
-                                </Form.Group>
-                                <Form.Group controlId="formCheckbox">
-                                    <Form.Check type="checkbox" label="Remember me on this device" style={{color:"white"}} />
-                                </Form.Group>
-            
-                                <Row style={{justifyContent:"space-between", width:"80%"}}>
-                                    <p>I don't have an account yet - <a href= "/signup" style={{color: "#6153ae"}}> Sign Up</a></p>
-                                    <Button  variant="light" type="submit" style={buttonStyle}>
-                                        Next
-                                    </Button>
-                                </Row>
-                            </Form>
+                                <Form.Group controlId="formEmail">
+                                    <Form.Label>Email address</Form.Label>
+                                        <Form.Control 
+                                        type="email"
+                                        name="email"
+                                        value={values.email}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        style={{borderRadius:"10px", width:"80%"}} 
+                                        />
+                                        <Form.Text className="text-danger">{touched.email && errors.email}</Form.Text>
+                                    </Form.Group>
+                                    <Form.Group controlId="formPassword">
+                                        <Form.Label >Password</Form.Label>
+                                        <Form.Control 
+                                        type="password"
+                                        name="password"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.password}
+                                        style={{borderRadius:"10px", width:"80%"}} />
+                                        <Form.Text className="text-danger">{touched.password && errors.password}</Form.Text>
+                                    </Form.Group>
+                                    <Form.Group controlId="formCheckbox">
+                                        <Form.Check type="checkbox" label="Remember me on this device" style={{color:"white"}} />
+                                    </Form.Group>
+        
+                                    <Row style={{justifyContent:"space-between", width:"80%"}}>
+                                        <p>I don't have an account yet - <a href= "/signup" style={{color: "#6153ae"}}> Sign Up</a></p>
+                                        <Button  variant="light" type="submit" disabled={isSubmitting} style={buttonStyle}>
+                                            Next
+                                        </Button>
+                                    </Row>
+                                </Form>
+                            )}
+                        </Formik>
+                            
                         </Col>  
                         
                     </Col>
