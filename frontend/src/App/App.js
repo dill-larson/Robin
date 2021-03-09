ae606ae84f64cacfd0d41e6d273d6b1c389794c6
dill-larson <dillonlouislarson@gmail.com>
2021-03-09 12:09:33 -0800

    added routing to education input

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -5,12 +5,13 @@ import './App.scss';
 
 import Home from '../Home/Home';
 import SignUp from '../SignUp/SignUp';
-import Login from '../login/Login'
-import GeneralOnboarding from '../SignUp/GeneralOnboarding'
-import EducationHome from '../SignUp/EducationOnboarding'
-import ExperienceHome from '../SignUp/ExperienceOnboarding'
-import Skills from '../SignUp/SkillsOnboarding'
-import ProjectsHome from '../SignUp/ProjectsOnboarding'
+import Login from '../Login/Login';
+import GeneralOnboarding from '../SignUp/GeneralOnboarding';
+import EducationHome from '../SignUp/EducationOnboarding';
+import EducationInput from '../SignUp/EducationInputOnboarding';
+import ExperienceHome from '../SignUp/ExperienceOnboarding';
+import Skills from '../SignUp/SkillsOnboarding';
+import ProjectsHome from '../SignUp/ProjectsOnboarding';
 
 
 function App() {
@@ -22,7 +23,7 @@ function App() {
             <Route exact path="/login" component={Login}/>
             <Route exact path="/onboarding-general" component={GeneralOnboarding}/>
             <Route exact path="/onboarding-education-1" component={EducationHome}/>
-            <Route exact path="/onboarding-education-2" component={GeneralOnboarding}/>
+            <Route exact path="/onboarding-education-2" component={EducationInput}/>
             <Route exact path="/onboarding-education-3" component={GeneralOnboarding}/>
             <Route exact path="/onboarding-experience-1" component={ExperienceHome}/>
             <Route exact path="/onboarding-experience-2" component={GeneralOnboarding}/>
