66c218debc0338903ad1a1b2e305d1bbdfc99e0a
dill-larson <dillonlouislarson@gmail.com>
2021-03-09 12:00:32 -0800

    ✨ created education input for onboarding

---
+++ frontend/src/SignUp/EducationInputOnboarding.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Formik } from 'formik';
+import * as yup from 'yup';
+
+import OnboardingMenu from '../Components/OnboardingMenu';
+
+import Page from '../Page/Page';
+import Logo from "../illustrations/Logo";
+
+export default class EducationOnboarding extends React.Component {
+    
+
+    render() {
+        return(null);
+    }
+}
