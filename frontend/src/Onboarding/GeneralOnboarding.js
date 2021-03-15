db69bad790f94000a16e67120f5db1609fcb19bf
dill-larson <dillonlouislarson@gmail.com>
2021-03-15 16:56:21 -0700

    💄 adjusted styling; should be responsive

--- frontend/src/Onboarding/GeneralOnboarding.js
+++ frontend/src/Onboarding/GeneralOnboarding.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import { Button, Col, Form, Row, InputGroup,FormControl } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import { Formik } from 'formik';
 import * as yup from 'yup';
-import Nav from "../Components/Nav"
 import './Onboarding.scss';
 
-import OnboardingMenu from './OnboardingMenu'
-
-import Page from '../Page/Page';
-import Logo from "../illustrations/Logo"
-import Checklist from '../illustrations/Checklist';
-import UserPool from '../UserPool';
+import Logo from "../illustrations/Logo";
 
 export default class GeneralOnboarding extends React.Component {
-
     validationSchema = yup.object({
         name: yup.string()
             .required("Required"),
@@ -22,11 +14,10 @@ export default class GeneralOnboarding extends React.Component {
             .email("Invalid email")
             .required("Required"),
         phone: yup.number("No special characters")
-            .min(1000000000, "Please input a valid phone number, no special characters")
+            .positive("Invalid US phone number")
             .required("Required"),
-        website: yup.string().url("Please input a valid url"),
-        // tos: yup.bool()
-        //     .required("Required")
+        website: yup.string()
+            .url("Invalid url")
     });
 
     constructor(props) {
@@ -34,144 +25,138 @@ export default class GeneralOnboarding extends React.Component {
     }
 
     handleSubmit(value) {
-
+        this.props.onUserDataUpdate(value, "general");
     }
 
     render() {
         return (
-        <Page>
-            <Row>
-                <Col md={4}> 
-                <OnboardingMenu current="general"></OnboardingMenu>
-                </Col>
-                <Col>
-                    <Row style={{justifyContent:"space-between"}}>
-                        <h1 className="onboarding-title">General Information</h1>
-                        <Logo size="12rem" ></Logo>
-                    </Row>
-                    <Formik
-                            initialValues={{
-                                name: '',
-                                phone: '',
-                                email: '',
-                                website: '',
-                                linkedin: '',
-                                github: '',
-                                
-                            }}
-                            validationSchema={this.validationSchema}
-                            onSubmit={(values) => (this.handleSubmit(values))}
-                        >
-                            {({
-                                handleSubmit,
-                                handleChange,
-                                handleBlur,
-                                values,
-                                touched,
-                                errors,
-                            }) => (
-                                <Form className="onboarding-general-form" onSubmit={handleSubmit}>
-                                    <Form.Row>
-                                        <Form.Group controlId="name" style={{width:"60%", marginRight: "3rem"}}>
-                                            <Form.Label className="onboarding-form-label">Full name</Form.Label>
-                                            <Form.Control
-                                                className="onboarding-form-input"
-                                                type="text"
-                                                value={values.name}
-                                                onChange={handleChange}
-                                                onBlur={handleBlur}
-                                                isValid={touched.name && !errors.name}
-                                                isInvalid={touched.name && errors.name}
-                                            />
-                                            <Form.Text className="form-error">{touched.name && errors.name}</Form.Text>
-                                        </Form.Group>
-                                        <Form.Group controlId="phone" style={{width:"33%"}}>
-                                            <Form.Label className="onboarding-form-label">Phone Number</Form.Label>
-                                            <Form.Control
-                                                className="onboarding-form-input"
-                                                type="text"
-                                                value={values.phone}
-                                                onChange={handleChange}
-                                                onBlur={handleBlur}
-                                                isValid={touched.phone && !errors.phone}
-                                                isInvalid={ touched.phone && errors.phone}
-                                            />
-                                            <Form.Text className="form-error">{touched.phone && errors.phone}</Form.Text>
-                                        </Form.Group>
-                                    </Form.Row>
-                                
-                                    <Form.Group controlId="email">
-                                        <Form.Label className="onboarding-form-label">Email</Form.Label>
-                                        <Form.Control
-                                            className="onboarding-form-input"
-                                            type="email"
-                                            value={values.email}
-                                            onChange={handleChange}
-                                            onBlur={handleBlur}
-                                            isValid={touched.email && !errors.email}
-                                            isInvalid={touched.email && errors.email}
-                                        />
-                                        <Form.Text className="form-error">{touched.email && errors.email}</Form.Text>
-                                    </Form.Group>
-                                    <Form.Group controlId="website">
-                                        <Form.Label className="onboarding-form-label">Personal Website</Form.Label>
-                                        <Form.Control
-                                            className="onboarding-form-input"
-                                            type="url"
-                                            placeholder="http://www.personal-website.com"
-                                            value={values.website}
-                                            onChange={handleChange}
-                                            onBlur={handleBlur}
-                                            isValid={touched.webstie && !errors.website}
-                                            isInvalid={touched.website && errors.website}
-                                        />
-                                        <Form.Text className="form-error">{touched.website && errors.website}</Form.Text>
-                                    </Form.Group>
-                                    <Row style={{justifyContent:"space-between"}}>
-                                        <Form.Group style={{marginLeft:"1rem", width:"45%"}}>
-                                            <Form.Label className="onboarding-form-label">LinkedIn</Form.Label>
-                                            <InputGroup 
-                                            className="onboarding-form-input">
-                                                <InputGroup.Prepend>
-                                                <InputGroup.Text>@</InputGroup.Text>
-                                                </InputGroup.Prepend>
-                                                <FormControl 
-                                                value={values.linkedin}
-                                                onChange={handleChange}
-                                                onBlur={handleBlur} 
-                                                placeholder="Username" />
-                                            </InputGroup>
-                                            
-                                        </Form.Group>
-                                        <Form.Group  style={{marginRight:"1rem", width:"45%"}}>
-                                            <Form.Label className="onboarding-form-label">Github</Form.Label>
-                                            <InputGroup 
-                                            className="onboarding-form-input">
-                                                <InputGroup.Prepend>
-                                                <InputGroup.Text>@</InputGroup.Text>
-                                                </InputGroup.Prepend>
-                                                <FormControl 
-                                                value={values.github}
-                                                onChange={handleChange}
-                                                onBlur={handleBlur} 
-                                                placeholder="Username" />
-                                            </InputGroup>
-
-                                        </Form.Group>
-                                
-                                        
-                                    </Row>
-                                    <Row style={{justifyContent:"flex-end"}}>
-                                        <Button className="onboarding-submit" type="submit">Next</Button>
-                                    </Row>
-                                </Form>
-                            )}
-                        </Formik>
-                </Col>
-
+        <div>
+            <Row style={{justifyContent:"space-between"}}>
+                <h1 className="onboarding-title">General Information</h1>
+                <Logo size="12rem" ></Logo>
             </Row>
-           
-        </Page>
+            <Formik
+                    initialValues={{
+                        name: '',
+                        phone: '',
+                        email: '',
+                        website: '',
+                        linkedin: '',
+                        github: '',
+                        
+                    }}
+                    validationSchema={this.validationSchema}
+                    onSubmit={(values) => (this.handleSubmit(values))}
+                >
+                    {({
+                        handleSubmit,
+                        handleChange,
+                        handleBlur,
+                        values,
+                        touched,
+                        errors,
+                    }) => (
+                        <Form className="onboarding-form" onSubmit={handleSubmit}>
+                            <Form.Row>
+                                <Form.Group as={Col} md={8} controlId="name">
+                                    <Form.Label className="onboarding-form-label">Full name</Form.Label>
+                                    <Form.Control
+                                        className="onboarding-form-input"
+                                        type="text"
+                                        value={values.name}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        isValid={touched.name && !errors.name}
+                                        isInvalid={touched.name && errors.name}
+                                    />
+                                    <Form.Text className="form-error">{touched.name && errors.name}</Form.Text>
+                                </Form.Group>
+                                <Form.Group as={Col} controlId="phone">
+                                    <Form.Label className="onboarding-form-label">Phone Number</Form.Label>
+                                    <Form.Control
+                                        className="onboarding-form-input"
+                                        type="text"
+                                        value={values.phone}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        isValid={touched.phone && !errors.phone}
+                                        isInvalid={ touched.phone && errors.phone}
+                                    />
+                                    <Form.Text className="form-error">{touched.phone && errors.phone}</Form.Text>
+                                </Form.Group>
+                            </Form.Row>
+                            <Form.Row>
+                                <Form.Group as={Col} controlId="email">
+                                    <Form.Label className="onboarding-form-label">Email</Form.Label>
+                                    <Form.Control
+                                        className="onboarding-form-input"
+                                        type="email"
+                                        value={values.email}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        isValid={touched.email && !errors.email}
+                                        isInvalid={touched.email && errors.email}
+                                    />
+                                    <Form.Text className="form-error">{touched.email && errors.email}</Form.Text>
+                                </Form.Group>
+                                <Form.Group as={Col} controlId="website">
+                                    <Form.Label className="onboarding-form-label">Personal Website</Form.Label>
+                                    <Form.Control
+                                        className="onboarding-form-input"
+                                        type="url"
+                                        placeholder="http://www.personal-website.com"
+                                        value={values.website}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        isValid={touched.webstie && !errors.website}
+                                        isInvalid={touched.website && errors.website}
+                                    />
+                                    <Form.Text className="form-error">{touched.website && errors.website}</Form.Text>
+                                </Form.Group>
+                            </Form.Row>
+                            <Form.Row>
+                                <Form.Group as={Col}>
+                                    <Form.Label className="onboarding-form-label">LinkedIn</Form.Label>
+                                    <InputGroup className="onboarding-form-input">
+                                        <InputGroup.Prepend>
+                                        <InputGroup.Text>@</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl 
+                                            value={values.linkedin}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur} 
+                                            placeholder="Username"
+                                        />
+                                    </InputGroup>
+                                </Form.Group>
+                                <Form.Group as={Col}>
+                                    <Form.Label className="onboarding-form-label">Github</Form.Label>
+                                    <InputGroup className="onboarding-form-input">
+                                        <InputGroup.Prepend>
+                                        <InputGroup.Text>@</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl 
+                                            value={values.github}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur} 
+                                            placeholder="Username"
+                                        />
+                                    </InputGroup>
+                                </Form.Group>
+                            </Form.Row>
+                            <Row>
+                                <Button 
+                                    type="submit"
+                                    variant="light-accent" 
+                                    className="onboarding-form-btn text-white ml-auto"
+                                >
+                                    Next
+                                </Button>
+                            </Row>
+                        </Form>
+                    )}
+                </Formik>
+        </div>
         )
     }
 }
