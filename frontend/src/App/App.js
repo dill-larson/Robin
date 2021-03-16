b37c0704f96150b104ef9e93f0139eb6a626a02f
dill-larson <dillonlouislarson@gmail.com>
2021-03-15 18:00:29 -0700

    moved routing into onboardingPage

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route,Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import {Row} from 'react-bootstrap';
 import './App.scss';
 
 import Home from '../Home/Home';
 import SignUp from '../SignUp/SignUp';
 import Login from '../Login/Login';
-import GeneralOnboarding from '../SignUp/GeneralOnboarding';
-import EducationHome from '../SignUp/EducationOnboarding';
-import EducationInput from '../SignUp/EducationInputOnboarding';
-import EducationExtras from '../SignUp/EducationExtrasOnboarding';
-import ExperienceHome from '../SignUp/ExperienceOnboarding';
-import ExperienceInput from '../SignUp/ExperienceInputOnboarding';
-import Skills from '../SignUp/SkillsOnboarding';
-import ProjectsHome from '../SignUp/ProjectsOnboarding';
+import OnboardingPage from '../Onboarding/OnboardingPage';
 
 
 function App() {
@@ -23,15 +16,7 @@ function App() {
             <Route exact path="/" component={Home}/>
             <Route exact path="/signup" component={SignUp}/>
             <Route exact path="/login" component={Login}/>
-            <Route exact path="/onboarding-general" component={GeneralOnboarding}/>
-            <Route exact path="/onboarding-education-1" component={EducationHome}/>
-            <Route exact path="/onboarding-education-2" component={EducationInput}/>
-            <Route exact path="/onboarding-education-3" component={EducationExtras}/>
-            <Route exact path="/onboarding-experience-1" component={ExperienceHome}/>
-            <Route exact path="/onboarding-experience-2" component={ExperienceInput}/>
-            <Route exact path="/onboarding-skills" component={Skills}/>
-            <Route exact path="/onboarding-projects-1" component={ProjectsHome}/>
-            <Route exact path="/onboarding-projects-2" component={GeneralOnboarding}/>
+            <Route path="/onboarding" component={OnboardingPage}/>
         </Switch>
     </Router>
   );
