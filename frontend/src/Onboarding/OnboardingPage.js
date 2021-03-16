a91060798f36ee1ce6e44b9da885a7d080121621
dill-larson <dillonlouislarson@gmail.com>
2021-03-15 18:01:18 -0700

    added routing for educationHomepage

--- frontend/src/Onboarding/OnboardingPage.js
+++ frontend/src/Onboarding/OnboardingPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import { Switch, Route, useRouteMatch } from 'react-router-dom';
 
+import EducationOnboarding from './EducationOnboarding';
 import GeneralOnboarding from './GeneralOnboarding';
 import OnboardingMenu from './OnboardingMenu';
 import Page from '../Page/Page';
@@ -23,15 +24,17 @@ export default function OnboardingPage(props) {
     return(
         <Page>
             <Row>
-                <Col md={4}> 
+                <Col md={4} className="mt-5"> 
                     <OnboardingMenu progress={progress} url={url}/>
                 </Col>
-                <Col>
+                <Col className="mt-5">
                     <Switch>
                         <Route path={`${path}/general`}>
                             <GeneralOnboarding onUserDataUpdate={handleUserDataUpdate}/>
                         </Route>
-                        <Route path={`${path}/education`} component={GeneralOnboarding} />
+                        <Route path={`${path}/education`}>
+                            <EducationOnboarding onUserDataUpdate={handleUserDataUpdate}/>
+                        </Route>
                         <Route path={`${path}/experience`} component={GeneralOnboarding} />
                         <Route path={`${path}/skills`} component={GeneralOnboarding} />
                         <Route path={`${path}/projects`} component={GeneralOnboarding} />
