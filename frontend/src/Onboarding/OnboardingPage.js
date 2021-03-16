0b7e06aae417b19a107abcc433c7b0b0cdc7eba7
dill-larson <dillonlouislarson@gmail.com>
2021-03-15 19:24:58 -0700

    added routing for input pages

--- frontend/src/Onboarding/OnboardingPage.js
+++ frontend/src/Onboarding/OnboardingPage.js
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import { Switch, Route, useRouteMatch } from 'react-router-dom';
 
+import ProjectsInputOnboarding from './ProjectInputOnboarding';
 import ProjectsOnboarding from './ProjectsOnboarding';
 import SkillsOnboarding from './SkillsOnboarding';
+import ExperienceInputOnboarding from './ExperienceInputOnboarding';
 import ExperienceOnboarding from './ExperienceOnboarding';
+import EducationInputOnboarding from './EducationInputOnboarding';
 import EducationOnboarding from './EducationOnboarding';
 import GeneralOnboarding from './GeneralOnboarding';
 import OnboardingMenu from './OnboardingMenu';
@@ -35,15 +38,21 @@ export default function OnboardingPage(props) {
                         <Route path={`${path}/general`}>
                             <GeneralOnboarding onUserDataUpdate={handleUserDataUpdate}/>
                         </Route>
+                        <Route path={`${path}/education/create`} component={EducationInputOnboarding}/>
+                        <Route path={`${path}/education/:eduId`} component={EducationInputOnboarding}/>
                         <Route path={`${path}/education`}>
                             <EducationOnboarding onUserDataUpdate={handleUserDataUpdate}/>
                         </Route>
+                        <Route path={`${path}/experience/create`} component={ExperienceInputOnboarding}/>
+                        <Route path={`${path}/experience/:jobId`} component={ExperienceInputOnboarding}/>
                         <Route path={`${path}/experience`}>
                             <ExperienceOnboarding onUserDataUpdate={handleUserDataUpdate}/>
                         </Route>
                         <Route path={`${path}/skills`}>
                             <SkillsOnboarding onUserDataUpdate={handleUserDataUpdate}/>
                         </Route>
+                        <Route path={`${path}/projects/create`} component={ProjectsInputOnboarding}/>
+                        <Route path={`${path}/experience/:projectId`} component={ProjectsInputOnboarding}/>
                         <Route path={`${path}/projects`}>
                             <ProjectsOnboarding onUserDataUpdate={handleUserDataUpdate}/>
                         </Route>
