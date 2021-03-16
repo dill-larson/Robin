5006cd3c166cff09588c36da440f50fe97a6a10f
dill-larson <dillonlouislarson@gmail.com>
2021-03-15 17:39:28 -0700

    💄 changed styling to use cards

--- frontend/src/Onboarding/EducationCard.js
+++ frontend/src/Onboarding/EducationCard.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { Row, Button,Col } from 'react-bootstrap';
+import { Button, Card, Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-function Field(props){
-    return( <Row className= "information-field" style={{justifyContent:"flex-end"}}>
-        <Col style={{marginTop:"1.5rem"}}>
-        <h3 >{props.name}</h3>
-        <p>{props.description}</p>
-        </Col>
-        <Button type="submit" style={{height:"2.3rem", marginTop:"3rem",marginBottom:"3rem",marginRight:"1rem", backgroundColor:"grey", borderColor:"grey"}}>Edit</Button>
-</Row>);
-}
-export default function InformationFields(props) {
-    return( 
-    <div className= "information-field-back">
-       <Field name={props.name} description={props.description}></Field>
-       <Row  style={{justifyContent:"flex-end", width:"45rem"}}>
-        <Button type="submit" style={{height:"2.3rem", backgroundColor:"grey", borderColor:"grey"}}>Add</Button>
-       </Row>
-    </div>);
-   
-    
+export default function EducationCard(props) {
+    return ( 
+        <Card style={{width: "100%"}}>
+            <Row>
+                <Col md={10}>
+                    <Card.Body>
+                        <Card.Title>{props.degree} in <span className="text-main-brand">{props.field_of_study}</span></Card.Title>
+                        <Card.Subtitle>{props.school} - {props.start_date} to {props.graduation_date}</Card.Subtitle>
+                        <Card.Subtitle className="mt-2">GPA: {props.gpa}</Card.Subtitle>
+                    </Card.Body>
+                </Col>
+                <Col className="mt-3">
+                    <Button 
+                        variant="light-shade" 
+                        className="onboarding-form-btn ml-auto"
+                    >
+                        Edit
+                    </Button>
+                </Col>
+            </Row>
+        </Card>
+    );
 }
