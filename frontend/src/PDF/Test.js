92d88ced3e359e6372df41b330cadd637fedca98
dill-larson <dillonlouislarson@gmail.com>
2021-04-26 16:17:39 -0700

    updated function call

--- frontend/src/PDF/Test.js
+++ frontend/src/PDF/Test.js
@@ -13,5 +13,5 @@ export default function Test() {
 
 function handleClick(e) {
     e.preventDefault();
-    createResume("clouduser@gmail.com");
+    createResume("clouduser@gmail.com", "Strong hands-on experience with Java Experience with MongoDB, Kafka Problem solving skills %26 technical troubleshooting Experience with testing frameworks, continuous integration and build tools");
 }
