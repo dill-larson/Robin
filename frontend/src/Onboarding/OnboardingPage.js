14a7ce6954084d616bb9421ea38d73ce60f2fbf2
laurahernandezr <lvhernandezrincon@gmail.com>
2021-05-04 19:56:40 -0700

    added sessions, allowed users to navigate pages only when logged in, and connected the creation of the pdf to the job search

--- frontend/src/Onboarding/OnboardingPage.js
+++ frontend/src/Onboarding/OnboardingPage.js
@@ -12,12 +12,13 @@ import EducationOnboarding from './EducationOnboarding';
 import GeneralOnboarding from './GeneralOnboarding';
 import OnboardingMenu from './OnboardingMenu';
 import Page from '../Page/Page';
+import Home from '../Home/Home';
 
 export default function OnboardingPage(props) {
     const [progress, setProgress] = useState([0]);
     const [from, setFrom] = useState([]);
     let { path, url } = useRouteMatch();
-    
+    let loggedIn = sessionStorage.getItem('loggedIn');
 
     function updateProgress() {
         setProgress(progress => [...progress, progress.length]);
@@ -29,39 +30,42 @@ export default function OnboardingPage(props) {
             setFrom(from => [...from, origin]);
         }
     }
-
-    return(
-        <Page>
-            <Row>
-                <Col md={4} className="mt-5"> 
-                    <OnboardingMenu progress={progress} url={url}/>
-                </Col>
-                <Col className="mt-5">
-                    <Switch>
-                        <Route path={`${path}/general`}>
-                            <GeneralOnboarding onUserDataUpdate={handleUserDataUpdate}/>
-                        </Route>
-                        <Route path={`${path}/education/create`} component={EducationInputOnboarding}/>
-                        <Route path={`${path}/education/:eduId`} component={EducationInputOnboarding}/>
-                        <Route path={`${path}/education`}>
-                            <EducationOnboarding onUserDataUpdate={handleUserDataUpdate}/>
-                        </Route>
-                        <Route path={`${path}/experience/create`} component={ExperienceInputOnboarding}/>
-                        <Route path={`${path}/experience/:jobId`} component={ExperienceInputOnboarding}/>
-                        <Route path={`${path}/experience`}>
-                            <ExperienceOnboarding onUserDataUpdate={handleUserDataUpdate}/>
-                        </Route>
-                        <Route path={`${path}/skills`}>
-                            <SkillsOnboarding onUserDataUpdate={handleUserDataUpdate}/>
-                        </Route>
-                        <Route path={`${path}/projects/create`} component={ProjectsInputOnboarding}/>
-                        <Route path={`${path}/projects/:projectId`} component={ProjectsInputOnboarding}/>
-                        <Route path={`${path}/projects`}>
-                            <ProjectsOnboarding onUserDataUpdate={handleUserDataUpdate}/>
-                        </Route>
-                    </Switch>
-                </Col>
-            </Row>
-        </Page>
-    );
+    if(loggedIn === "true"){
+        return(
+            <Page>
+                <Row>
+                    <Col md={4} className="mt-5"> 
+                        <OnboardingMenu progress={progress} url={url}/>
+                    </Col>
+                    <Col className="mt-5">
+                        <Switch>
+                            <Route path={`${path}/general`}>
+                                <GeneralOnboarding onUserDataUpdate={handleUserDataUpdate}/>
+                            </Route>
+                            <Route path={`${path}/education/create`} component={EducationInputOnboarding}/>
+                            <Route path={`${path}/education/:eduId`} component={EducationInputOnboarding}/>
+                            <Route path={`${path}/education`}>
+                                <EducationOnboarding onUserDataUpdate={handleUserDataUpdate}/>
+                            </Route>
+                            <Route path={`${path}/experience/create`} component={ExperienceInputOnboarding}/>
+                            <Route path={`${path}/experience/:jobId`} component={ExperienceInputOnboarding}/>
+                            <Route path={`${path}/experience`}>
+                                <ExperienceOnboarding onUserDataUpdate={handleUserDataUpdate}/>
+                            </Route>
+                            <Route path={`${path}/skills`}>
+                                <SkillsOnboarding onUserDataUpdate={handleUserDataUpdate}/>
+                            </Route>
+                            <Route path={`${path}/projects/create`} component={ProjectsInputOnboarding}/>
+                            <Route path={`${path}/projects/:projectId`} component={ProjectsInputOnboarding}/>
+                            <Route path={`${path}/projects`}>
+                                <ProjectsOnboarding onUserDataUpdate={handleUserDataUpdate}/>
+                            </Route>
+                        </Switch>
+                    </Col>
+                </Row>
+            </Page>
+        );
+    }else{
+        return(<Home></Home>)
+    }
 }
