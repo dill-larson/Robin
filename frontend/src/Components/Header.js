91e7018cedab109a56e9a73766faa2c275fcb018
dill-larson <dillonlouislarson@gmail.com>
2021-05-06 13:30:15 -0700

    💄 fixed row and col issues; should have proper margins

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Row } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import Logo from '../illustrations/Logo';
-import SignupNav from '../Components/SignupNav'
+import SignupNav from '../Components/SignupNav';
 
 export default function Header(props) {
     return (
-        <>
-          <Row style={{justifyContent:"space-between"}}>
-              <Logo size="12rem"></Logo>
-              <SignupNav></SignupNav>
-          </Row>
-        </>
+        <Col>
+            <Row>
+                <Logo size="12rem"></Logo>
+                <SignupNav/>
+            </Row>
+        </Col>
     );
 }
