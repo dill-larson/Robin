3bad244c1840ee902a786496a2b76ac6abed3cfc
dill-larson <dillonlouislarson@gmail.com>
2021-05-06 13:51:51 -0700

    💄 updated styling for homepage

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -3,11 +3,11 @@ import { Col, Row } from 'react-bootstrap';
 import Logo from '../illustrations/Logo';
 import SignupNav from '../Components/SignupNav';
 
-export default function Header(props) {
+export default function Header() {
     return (
         <Col>
             <Row>
-                <Logo size="12rem"></Logo>
+                <Logo size="12rem" />
                 <SignupNav/>
             </Row>
         </Col>
