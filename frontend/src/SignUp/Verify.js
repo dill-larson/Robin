49d5e224bcf0a164590b492b8fc3d5d0b77f86c4
dill-larson <dillonlouislarson@gmail.com>
2021-05-06 14:40:55 -0700

    💄 updated verify email styling

--- frontend/src/SignUp/Verify.js
+++ frontend/src/SignUp/Verify.js
@@ -1,25 +1,26 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+
+/* components */
 import Page from '../Page/Page'
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
-import { Col, Row } from 'react-bootstrap';
+
+/* svgs */
 import Illustration from '../illustrations/Mailbox_illustration'
 export default class Verify extends React.Component {
     render() {
         return(
             <Page>
-                <Header></Header>
-                <Row className="verify-email-information">
+                <Header/>
+                <Col className="verify-email-information text-center">
                     <h1>Email Verification</h1>
-                </Row>
-                <Row className="verify-email-information">
-                   <p>Please check your email for a verification link</p>
-                </Row>
-                <Row className="verify-email-information">
-                    <Illustration size="23rem"></Illustration>
-                </Row> 
-
-            
+                    <h5 className="text-light-accent">Please check your email for a verification link</h5>
+                    <Row style={{justifyContent: "center"}}>
+                        <Illustration size="23rem"></Illustration>
+                    </Row>
+                </Col>
+                <Footer/>
             </Page>
         );
     }
