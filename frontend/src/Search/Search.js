7923698a579efef609c6ef7df86a430d5d84a249
dill-larson <dillonlouislarson@gmail.com>
2021-05-12 08:58:46 -0700

    💄 updated styling

--- frontend/src/Search/Search.js
+++ frontend/src/Search/Search.js
@@ -10,16 +10,14 @@ import Results from '../Results/Results'
 import './Search.scss'
 import Home from '../Home/Home';
 import Login from '../Login/Login';
+import Page from '../Page/Page';
 
 export default class Search extends React.Component {
     
     validationSchema = yup.object({
-        position: yup.string(),
-        skills: yup.string(),
         url: yup.string()
-            .url("Invalid URL").required()
-            ,
-            
+            .required("Required")
+            .url("Invalid URL")
     });
 
     constructor(props) {
@@ -46,91 +44,56 @@ export default class Search extends React.Component {
     render() {
         if(this.state.loggedIn === "true"){
             if(this.state.search === true){
-            return(<Results searchUrl={this.state.url}></Results>)
+                return(<Results searchUrl={this.state.url}></Results>)
             }
             
             return(
-                <Container>
+                <Page>
                     <NavBar/>
                     <div className="search-card">
-                    <Formik
-                        initialValues={{
-                            skills: '',
-                            position: '',
-                            recommended: false
-        
-                        }}
-                        validationSchema={this.validationSchema}
-                        onSubmit={(values) => (this.handleSubmit(values))}
-                        >
-                        {({
-                            handleSubmit,
-                            handleChange,
-                            handleBlur,
-                            values,
-                            touched,
-                            errors,
-                        }) => (
-                            <Form onSubmit={handleSubmit}>
-                                <Form.Group controlId="url">
-                                    <Form.Label className="search-heddings" style={{color:'#6153ae'}}>Search job by position</Form.Label>
-                                    <Form.Control
-                                        type="text"
-                                        placeholder="URL"
-                                        value={values.url}
-                                        onChange={handleChange}
-                                        onBlur={handleBlur}
-                                        isValid={touched.url && !errors.url}
-                                        isInvalid={touched.url && errors.url}
-                                    />
-                                    <Form.Text className="text-danger">{touched.url && errors.url}</Form.Text>
-                                </Form.Group>
-                                <Button variant="light-accent text-white" className="search-button" type="submit">Search</Button>
-                                {/* <Form.Group controlId="skills">
-                                    <Form.Label className="search-heddings" style={{color:'#6153ae'}}>Search job by specific skills</Form.Label>
-                                    <Form.Control
-                                        className="onboarding-form-input"
-                                        type="text"
-                                        placeholder="skill1, skill2, skill3"
-                                        value={values.skills}
-                                        onChange={handleChange}
-                                        onBlur={handleBlur}
-                                        isValid={touched.skills && !errors.skills}
-                                        isInvalid={touched.skills && errors.skills}
-                                        
-                                    />
-                                    <Form.Text className="text-danger">{touched.skills && errors.skills}</Form.Text>
-                                </Form.Group>
-                                <Form.Group controlId="position">
-                                    <Form.Label className="search-heddings" style={{color:'#6153ae'}}>Search job by position</Form.Label>
-                                    <Form.Control
-                                        className="onboarding-form-input"
-                                        type="text"
-                                        placeholder="position"
-                                        value={values.position}
-                                        onChange={handleChange}
-                                        onBlur={handleBlur}
-                                        isValid={touched.position && !errors.position}
-                                        isInvalid={touched.position && errors.position}
-                                        
-                                    />
-                                    <Form.Text className="text-danger">{touched.position && errors.position}</Form.Text>
-                                </Form.Group> */}
-                                {/* <Row style={{justifyContent:"center", marginBottom:"2rem", marginTop:"2rem"}}>
-                                    <Button variant="light-accent text-white" className="search-button" type="submit">Search</Button>
-                                </Row>
-                                <Row style={{justifyContent:"center",marginBottom:"2rem", marginTop:"2rem"}}>
-                                    <h3 className="search-heddings"> -or- </h3><br></br>
-                                </Row>
-                                <Row style={{justifyContent:"center",marginBottom:"2rem", marginTop:"2rem"}}>
-                                    <Button  className="search-button" variant="light-accent text-white" type="submit"> Search recommended positions</Button>
-                                </Row> */}
-                            </Form>
-                        )}
-                    </Formik>
+                        <Formik
+                            initialValues={{
+                                skills: '',
+                                position: '',
+                                recommended: false
+            
+                            }}
+                            validationSchema={this.validationSchema}
+                            onSubmit={(values) => (this.handleSubmit(values))}
+                            >
+                            {({
+                                handleSubmit,
+                                handleChange,
+                                handleBlur,
+                                values,
+                                touched,
+                                errors,
+                            }) => (
+                                <Form className="search-form" onSubmit={handleSubmit}>
+                                    <div className="form-header">
+                                        <h1 className="form-title">Search for Jobs by URL</h1>
+                                    </div>
+                                    <Form.Group controlId="url">
+                                        <Form.Label className="form-label">LinkedIn URL</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            placeholder="URL"
+                                            value={values.url}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            isValid={touched.url && !errors.url}
+                                            isInvalid={touched.url && errors.url}
+                                        />
+                                        <Form.Text className="form-error-text">{touched.url && errors.url}</Form.Text>
+                                    </Form.Group>
+                                    <div className="form-footer">
+                                        <Button variant="light-accent" className="text-white ml-auto" type="submit">Search</Button>
+                                    </div>
+                                </Form>
+                            )}
+                        </Formik>
                     </div>
-                    
-                </Container>
+                </Page>
             );
         } else {
             return(<Login></Login>)
