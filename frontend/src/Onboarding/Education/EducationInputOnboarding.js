eb884bade337d69a49bb1e7d9c90f2b9bd2f7fc2
dill-larson <dillonlouislarson@gmail.com>
2021-05-11 19:06:32 -0700

    💄 restyled form. added extras with collapse btn

--- frontend/src/Onboarding/Education/EducationInputOnboarding.js
+++ frontend/src/Onboarding/Education/EducationInputOnboarding.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { Button, Col, Form, Row } from 'react-bootstrap';
-import { Link, Redirect } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Accordion, AccordionCollapse, Button, Card, Col, Form, Row } from 'react-bootstrap';
+import { Link, Redirect, Route, Switch, useRouteMatch } from 'react-router-dom';
 import { Formik } from 'formik';
 import * as yup from 'yup';
 import '../Onboarding.scss';
 
-import Logo from "../../illustrations/Logo";
 import axios from 'axios';
 
+let avg_chars_per_word = 6;
+let max_words = 13;
 export default class EducationInputOnboarding extends React.Component {
     validationSchema = yup.object({
         school: yup.string()
@@ -16,53 +17,68 @@ export default class EducationInputOnboarding extends React.Component {
             .required("Required"),
         field_of_study: yup.string()
             .required("Required"),
-        start_date: yup.string()
+        start_date: yup.date()
             .required("Required"),
         graduation_date: yup.date()
+            .min(yup.ref('start_date'), "Date must be later than start date")
             .required("Required"),
         gpa: yup.number()
             .moreThan(0.9 ,"GPA must be higher to equal to 1.0")
             .lessThan(5.1, "GPA must be lower or equal to 5.0")
-            .required("Required")
+            .required("Required"),
+        honors: yup.string()
+            .max(max_words * avg_chars_per_word),
+        rel_course_work: yup.string()
+            .max(max_words * avg_chars_per_word),
+        activities: yup.string()
+            .max(max_words * avg_chars_per_word)
     });
 
     constructor(props) {
         super(props);
         this.state = {
-            informationPosted: false
+            informationPosted: false,
+            showExtras: false
         };
     }
 
     handleSubmit(values) {
-        const degree = {
-            school: values.school,
-            degree: values.degree, 
-            field_of_study: values.field_of_study,
-            start_date: values.start_date,
-            graduation_date: values.graduation_date,
-            gpa:values.gpa
-        }
-        axios.post('http://127.0.0.1:5000/onboard/education',{degree})
-            .then(res => {
-                console.log(res.data);
-                this.setState({informationPosted: true});
-            })
-            .catch(err => {
-                console.log(err);
-            })
-        this.setState({informationPosted: true});
+        console.log(values);
+        // const degree = {
+        //     school: values.school,
+        //     degree: values.degree, 
+        //     field_of_study: values.field_of_study,
+        //     start_date: values.start_date,
+        //     graduation_date: values.graduation_date,
+        //     gpa: values.gpa,
+        //     honors: values.honors,
+        //     rel_course_work: values.rel_course_work, 
+        //     activities: values.activities
+        // }
+        // axios.post('http://127.0.0.1:5000/onboard/education',{degree})
+        //     .then(res => {
+        //         console.log(res.data);
+        //         this.setState({informationPosted: true});
+        //     })
+        //     .catch(err => {
+        //         console.log(err);
+        //     })
+        // this.setState({informationPosted: true});
+    }
+
+    handleExtras() {
+        this.setState({showExtras: true});
     }
 
     render() {
         if(this.state.informationPosted){
             return <Redirect to='/onboarding/education'/>
         }
+        if(this.state.showExtras) {
+            return <Redirect to='/onboarding/education/create/extras' />
+        }
         return(
             <div>
-                <Row style={{justifyContent:"space-between"}}>
-                    <h1 className="onboarding-title">Degree</h1>
-                    <Logo size="12rem" ></Logo>
-                </Row>
                 <Formik
                     initialValues={{
                         school: '',
@@ -70,7 +86,10 @@ export default class EducationInputOnboarding extends React.Component {
                         field_of_study: '',
                         start_date: '',
                         graduation_date: '',
-                        gpa: ''
+                        gpa: '',
+                        honors: '',
+                        rel_course_work: '',
+                        activities: ''
                     }}
                     validationSchema={this.validationSchema}
                     onSubmit={(values) => (this.handleSubmit(values))}
@@ -81,13 +100,14 @@ export default class EducationInputOnboarding extends React.Component {
                         handleBlur,
                         values,
                         touched,
-                        errors,
+                        errors
                     }) => (
-                        <Form className="education-form" onSubmit={handleSubmit}>
+                        <Form className="onboarding-form" onSubmit={handleSubmit}>
+                            <h1 className="form-title">Degree</h1>
                             <Form.Group controlId="school">
-                                <Form.Label className="onboarding-form-label">School</Form.Label>
+                                <Form.Label className="form-label">School</Form.Label>
                                 <Form.Control
-                                    className="onboarding-form-input"
+                                    className="form-input"
                                     type="text"
                                     value={values.school}
                                     onChange={handleChange}
@@ -95,14 +115,14 @@ export default class EducationInputOnboarding extends React.Component {
                                     isValid={touched.school && !errors.school}
                                     isInvalid={touched.school && errors.school}
                                 />
-                                <Form.Text className="form-error">{touched.school && errors.school}</Form.Text>
+                                <Form.Text className="form-error-text">{touched.school && errors.school}</Form.Text>
                             </Form.Group>
                             <Form.Row>
                                 <Col>
                                     <Form.Group controlId="degree">
-                                        <Form.Label className="onboarding-form-label">Degree</Form.Label>
+                                        <Form.Label className="form-label">Degree</Form.Label>
                                         <Form.Control
-                                            className="onboarding-form-input"
+                                            className="form-input"
                                             type="text"
                                             value={values.degree}
                                             onChange={handleChange}
@@ -110,14 +130,14 @@ export default class EducationInputOnboarding extends React.Component {
                                             isValid={touched.degree && !errors.degree}
                                             isInvalid={touched.degree && errors.degree}
                                         />
-                                        <Form.Text className="form-error">{touched.degree && errors.degree}</Form.Text>
+                                        <Form.Text className="form-error-text">{touched.degree && errors.degree}</Form.Text>
                                     </Form.Group>
                                 </Col>
                                 <Col>
                                     <Form.Group controlId="field_of_study">
-                                        <Form.Label className="onboarding-form-label">Field of Study</Form.Label>
+                                        <Form.Label className="form-label">Field of Study</Form.Label>
                                         <Form.Control
-                                            className="onboarding-form-input"
+                                            className="form-input"
                                             type="text"
                                             value={values.field_of_study}
                                             onChange={handleChange}
@@ -125,16 +145,16 @@ export default class EducationInputOnboarding extends React.Component {
                                             isValid={touched.field_of_study && !errors.field_of_study}
                                             isInvalid={touched.field_of_study && errors.field_of_study}
                                         />
-                                        <Form.Text className="form-error">{touched.field_of_study && errors.field_of_study}</Form.Text>
+                                        <Form.Text className="form-error-text">{touched.field_of_study && errors.field_of_study}</Form.Text>
                                     </Form.Group>
                                 </Col>
                             </Form.Row>
                             <Form.Row>
                                 <Col>
                                     <Form.Group controlId="start_date">
-                                        <Form.Label className="onboarding-form-label">Start Date</Form.Label>
+                                        <Form.Label className="form-label">Start Date</Form.Label>
                                         <Form.Control
-                                            className="onboarding-form-input"
+                                            className="form-input"
                                             type="date"
                                             value={values.start_date}
                                             onChange={handleChange}
@@ -142,14 +162,14 @@ export default class EducationInputOnboarding extends React.Component {
                                             isValid={touched.start_date && !errors.start_date}
                                             isInvalid={touched.start_date && errors.start_date}
                                         />
-                                        <Form.Text className="form-error">{touched.start_date && errors.start_date}</Form.Text>
+                                        <Form.Text className="form-error-text">{touched.start_date && errors.start_date}</Form.Text>
                                     </Form.Group>
                                 </Col>
                                 <Col>
                                     <Form.Group controlId="graduation_date">
-                                        <Form.Label className="onboarding-form-label">Graduation Date</Form.Label>
+                                        <Form.Label className="form-label">Graduation Date</Form.Label>
                                         <Form.Control
-                                            className="onboarding-form-input"
+                                            className="form-input"
                                             type="date"
                                             value={values.graduation_date}
                                             onChange={handleChange}
@@ -157,14 +177,14 @@ export default class EducationInputOnboarding extends React.Component {
                                             isValid={touched.graduation_date && !errors.graduation_date}
                                             isInvalid={touched.graduation_date && errors.graduation_date}
                                         />
-                                        <Form.Text className="form-error">{touched.graduation_date && errors.graduation_date}</Form.Text>
+                                        <Form.Text className="form-error-text">{touched.graduation_date && errors.graduation_date}</Form.Text>
                                     </Form.Group>
                                 </Col>
                                 <Col>
                                     <Form.Group controlId="gpa">
-                                        <Form.Label className="onboarding-form-label">GPA</Form.Label>
+                                        <Form.Label className="form-label">GPA</Form.Label>
                                         <Form.Control
-                                            className="onboarding-form-input"
+                                            className="form-input"
                                             type="numbers"
                                             value={values.gpa}
                                             onChange={handleChange}
@@ -172,17 +192,63 @@ export default class EducationInputOnboarding extends React.Component {
                                             isValid={touched.gpa && !errors.gpa}
                                             isInvalid={touched.gpa && errors.gpa}
                                         />
-                                        <Form.Text className="form-error">{touched.gpa && errors.gpa}</Form.Text>
+                                        <Form.Text className="form-error-text">{touched.gpa && errors.gpa}</Form.Text>
                                     </Form.Group>
                                 </Col>
                             </Form.Row>
-                            <Row className="mb-2">
-                                <Button as={Link} to="/onboarding-education-3"variant="dark-shade" className="onboarding-form-btn ml-auto">Extras</Button>
-                            </Row>
-                            <Row>
-                                <Button variant="brand-danger" className="onboarding-form-btn ml-auto">Delete</Button>
-                                <Button variant="light-accent text-white" className="onboarding-form-btn ml-2" type="submit">Save</Button>
-                            </Row>
+                            <Accordion defaultActiveKey="0">
+                                <Card>
+                                    <Card.Header>
+                                        <Accordion.Toggle as={Button} className="ml-auto" variant="dark-shade" eventKey="0">Extras</Accordion.Toggle>
+                                    </Card.Header>
+                                    <Accordion.Collapse eventKey="0">
+                                        <Card.Body>
+                                            <Form.Group controlId="honors">
+                                                <Form.Label className="form-label">Honors</Form.Label>
+                                                <Form.Control
+                                                    as="textarea"
+                                                    className="form-input"
+                                                    value={values.honors}
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    isValid={touched.honors && !errors.honors}
+                                                    isInvalid={touched.honors && errors.honors}
+                                                />
+                                                <Form.Text className="form-error-text">{touched.honors && errors.honors}</Form.Text>
+                                            </Form.Group>
+                                            <Form.Group controlId="rel_course_work">
+                                                <Form.Label className="form-label">Relevant Course Work</Form.Label>
+                                                <Form.Control
+                                                    as="textarea"
+                                                    className="form-input"
+                                                    value={values.rel_course_work}
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    isValid={touched.rel_course_work && !errors.rel_course_work}
+                                                    isInvalid={touched.rel_course_work && errors.rel_course_work}
+                                                />
+                                                <Form.Text className="form-error-text">{touched.rel_course_work && errors.rel_course_work}</Form.Text>
+                                            </Form.Group>
+                                            <Form.Group controlId="activities">
+                                                <Form.Label className="form-label">Activities</Form.Label>
+                                                <Form.Control
+                                                    as="textarea"
+                                                    className="form-input"
+                                                    value={values.activities}
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    isValid={touched.activities && !errors.activities}
+                                                    isInvalid={touched.activities && errors.activities}
+                                                />
+                                                <Form.Text className="form-error-text">{touched.activities && errors.activities}</Form.Text>
+                                            </Form.Group>
+                                        </Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+                            </Accordion>
+                            <div className="form-footer">
+                                <Button variant="light-accent text-white" className="ml-auto" type="submit">Save</Button>
+                            </div>
                         </Form>
                     )}
                 </Formik>
