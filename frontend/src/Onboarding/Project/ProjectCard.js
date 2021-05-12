ac6927271210f9f5f3468c4429a96d200ac97d68
dill-larson <dillonlouislarson@gmail.com>
2021-05-11 20:52:03 -0700

    💄 updated styling to match other cards

--- frontend/src/Onboarding/Project/ProjectCard.js
+++ frontend/src/Onboarding/Project/ProjectCard.js
@@ -6,27 +6,17 @@ export default function ProjectCard(props) {
     return ( 
         <Card className="onboarding-card">
             <Row>
-                <Col md={7}>
+                <Col md={10}>
                     <Card.Body>
-                        <Card.Title><span className="text-main-brand">{props.name}</span></Card.Title>
+                        <Card.Title><span className="text-light-accent">{props.name}</span></Card.Title>
                         <Card.Subtitle>{props.start_date} to {props.end_date}</Card.Subtitle>
                         <Card.Body>{props.about}</Card.Body>
                     </Card.Body>
                 </Col>
-                <Col className="mt-3">
-                    <Button 
-                        variant="light-shade" 
-                        className="onboarding-form-btn"
-                    >
-                        Edit
-                    </Button>
-                    <Button 
-                        variant="brand-danger" 
-                        className="onboarding-form-btn mt-2"
-                    >
-                        Delete
-                    </Button>
-                </Col>
+                {/* <Col className="card-btns">
+                    <Button as={Link} to={`/onboarding/projects/edit/${props.id}`} className="mb-2" variant="light-shade">Edit</Button>
+                    <Button variant="danger">Delete</Button>
+                </Col> */}
             </Row>
         </Card>
     );
